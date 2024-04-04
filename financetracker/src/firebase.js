635163f0fc6ba83e59851f66b9fbf3e8bb0bc9c3
zijinn7 <e0969915@u.nus.edu>
2024-04-04 15:02:32 +0800

    deleted htmls and added firebase config

--- financetracker/src/firebase.js
+++ financetracker/src/firebase.js
@@ -2,12 +2,13 @@ import { initializeApp } from 'firebase/app';
 
 // Firebase project configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDCuDC9lJoxIH3W40BFLFWKrOA2M0FTEDI",
+  authDomain: "financetracker-6ff07.firebaseapp.com",
+  projectId: "financetracker-6ff07",
+  storageBucket: "financetracker-6ff07.appspot.com",
+  messagingSenderId: "751809296128",
+  appId: "1:751809296128:web:04b851f43d4f9ab211e5e5",
+  measurementId: "G-7KQEGS7VL1"
 };
 
 // Initialize Firebase app
