c679544a5719530a623f08dfda135b723a46c2dc
Liu Junzhe <122595783+liujunzhe2231@users.noreply.github.com>
2024-04-11 01:12:02 +0800

    Add files via upload

---
+++ financetracker/src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDCuDC9lJoxIH3W40BFLFWKrOA2M0FTEDI",
+    authDomain: "financetracker-6ff07.firebaseapp.com",
+    projectId: "financetracker-6ff07",
+    storageBucket: "financetracker-6ff07.appspot.com",
+    messagingSenderId: "751809296128",
+    appId: "1:751809296128:web:04b851f43d4f9ab211e5e5",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
+
+
+
---
+++ financetracker/src/uifire.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/compat/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDCuDC9lJoxIH3W40BFLFWKrOA2M0FTEDI",
+    authDomain: "financetracker-6ff07.firebaseapp.com",
+    projectId: "financetracker-6ff07",
+    storageBucket: "financetracker-6ff07.appspot.com",
+    messagingSenderId: "751809296128",
+    appId: "1:751809296128:web:04b851f43d4f9ab211e5e5",
+};
+
+firebase.initializeApp(firebaseConfig);
+console.log("fire is ", firebase)
+
+export default firebase;
