b74abca4a8832a82f38cd3354649611c8628af9d
zijinn7 <e0969915@u.nus.edu>
2024-04-15 13:42:12 +0800

    added donutchart component with basic template using vuechartkick

---
+++ financetracker/src/components/DonutChartSummary.vue
@@ -0,0 +1,38 @@
+<template>
+    <pie-chart :donut="true" :data="chartData" :round="2"></pie-chart>
+</template>
+
+<script>
+import { getFirestore, collection, addDoc } from 'firebase/firestore';
+// import { doc, setDoc } from "firebase/firestore";
+import firebaseApp from '../firebase.js';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import 'chartkick/chart.js'
+const db = getFirestore(firebaseApp);
+
+export default {
+    name: "DonutChartSummary",
+    mounted(){
+        const auth = getAuth();
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                this.user = user;
+                this.chartData = [["cat1", 4.4543], ["cat2", 5]];
+            }
+        })
+    },
+    data(){
+        return {
+            chartData: [], // Initialize chartData as an empty array
+            user: null,
+        };
+    },
+    methods(){
+
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
