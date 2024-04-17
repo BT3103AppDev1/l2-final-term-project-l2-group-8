79b39001d751659b7da3ceb7dbd3cd01a1e5f63f
Liu Junzhe <122595783+liujunzhe2231@users.noreply.github.com>
2024-04-18 01:59:08 +0800

    Add files via upload

--- financetracker/src/App.vue
+++ financetracker/src/App.vue
@@ -1,23 +1,18 @@
 <template>
-    <nav>
-        <router-link to="/">LogIn</router-link>|
-        <router-link to="/register">Register</router-link>|
-        <router-link to="/forum">Forum</router-link>
-    </nav>
-    <router-view/>
+    <body><router-view/></body>  
 </template>
 
 <script setup>
 
 </script>
 
-<style>
-#app {
-font-family: Avenir, Helvetica, Arial, sans-serif;
--webkit-font-smoothing: antialiased;
--moz-osx-font-smoothing: grayscale;
-text-align: center;
-color: #2c3e50;
-margin-top: 60px;
+<style scoped>
+#app {  
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
